afcce36569a488fc1bc041ffd8b5eb8e49aa0a5a
Eric Pauls <paulse@oregonstate.edu>
2021-10-28 14:40:35 -0400

    reformatted files

--- src/pages/CreateMealPlan.js
+++ src/pages/CreateMealPlan.js
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function CreateMealPlan() {
-    const history = useHistory()
+  const history = useHistory();
 
-    const goToEdit = () => {
-        history.push("/EditMealPlan")
-    }
+  const goToEdit = () => {
+    history.push("/EditMealPlan");
+  };
 
-    return (
-        
+  return (
+    <div>
+      <form>
+        <h1>Create Meal Plan</h1>
         <div>
-            <form>
-                <h1>Create Meal Plan</h1>
-                <div>
-                    <label for="name">Name:</label>
-                    <input type="text"/>
-                </div>
-                <button onClick={goToEdit}> Create Meal Plan</button>
-                
-            </form>
-            <Link to='/'>Return Home</Link><br/>
+          <label for="name">Name:</label>
+          <input type="text" />
         </div>
-    )
+        <button onClick={goToEdit}> Create Meal Plan</button>
+      </form>
+      <Link to="/">Return Home</Link>
+      <br />
+    </div>
+  );
 }
 
-export default CreateMealPlan
+export default CreateMealPlan;
